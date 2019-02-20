63e7dd72df973c7ff84e6194d6242e71259cdc3d
Michael Parisi <mikeparisi@gmail.com>
2019-02-20 13:46:17 -0800

    added carousel

--- src/routes/New.js
+++ src/routes/New.js
@@ -1,28 +1,77 @@
 import React from 'react';
-import { StyleSheet, View, TextInput } from 'react-native';
+import { StyleSheet, View, TextInput, Text, Dimensions } from 'react-native';
+import Carousel from '../components/Carousel';
 
 export default class New extends React.Component {
   state = {
-    text: ''
+    who: '',
+    what: '',
+    when: '',
+    where: ''
   };
 
   render() {
+    const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
+
     return (
-      <View>
-        <TextInput
-          style={styles.textInput}
-          onChangeText={(text) => this.setState({text})}
-          value={this.state.text}
-        />
-      </View>
-    );
+         <Carousel
+            style={[styles.container, {}]}
+            speed={500}
+            pagerColor="#000"
+            activePagerColor="#fff"
+          >
+            <View style={[styles.card]}>
+              <Text>Who?</Text>
+              <TextInput
+                style={styles.textInput}
+                onChangeText={(text) => this.setState({who: text})}
+                value={this.state.who}
+              />
+            </View>
+            <View style={[styles.card]}>
+              <Text>What?</Text>
+              <TextInput
+                style={styles.textInput}
+                onChangeText={(text) => this.setState({what: text})}
+                value={this.state.what}
+              />
+            </View>
+            <View style={[styles.card]}>
+              <Text>When?</Text>
+              <TextInput
+                style={styles.textInput}
+                onChangeText={(text) => this.setState({when: text})}
+                value={this.state.when}
+              />
+            </View>
+            <View style={[styles.card]}>
+              <Text>Where?</Text>
+              <TextInput
+                style={styles.textInput}
+                onChangeText={(text) => this.setState({where: text})}
+                value={this.state.where}
+              />
+            </View>
+          </Carousel>
+      );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: 500, 
+    height: 500
+  },
+  card: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   textInput: {
     borderWidth: 5,
-    borderColor: '#000'
+    borderColor: '#000',
+    width: '50%'
   },
 });
 
