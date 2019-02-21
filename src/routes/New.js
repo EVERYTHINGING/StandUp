bd717322c298c0ca4ed9a41964205dd9fcc1e52b
Michael Parisi <mikeparisi@gmail.com>
2019-02-20 21:51:22 -0800

    native carousel

--- src/routes/New.js
+++ src/routes/New.js
@@ -1,67 +1,118 @@
 import React from 'react';
-import { StyleSheet, View, TextInput, Text, Dimensions } from 'react-native';
-import Carousel from '../components/Carousel';
+import { StyleSheet, View, TextInput, Text, Dimensions, ScrollView, Button } from 'react-native';
 
 export default class New extends React.Component {
-  state = {
-    who: '',
-    what: '',
-    when: '',
-    where: ''
-  };
+  constructor(){
+    super();
+
+    this.state = {
+      who: '',
+      what: '',
+      when: '',
+      where: '',
+      currentCard: 0
+    };
+
+    this.scrollView = null;
+    this.handleNextPress = this.handleNextPress.bind(this);
+    this.handleBackPress = this.handleBackPress.bind(this);
+  }
+
+  handleNextPress(e){
+    const xPos = Dimensions.get('window').width*(this.state.currentCard+1);
+    this.refs._scrollView.scrollTo({x: xPos, y: 0, animated: true});
+
+    this.setState({ currentCard: this.state.currentCard+1 });
+  }
+
+  handleBackPress(e){
+    const xPos = Dimensions.get('window').width*(this.state.currentCard-1);
+    this.refs._scrollView.scrollTo({x: xPos, y: 0, animated: true});
+
+    this.setState({ currentCard: this.state.currentCard-1 });
+  }
 
   render() {
     const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
 
     return (
-         <Carousel
-            style={[styles.container, {}]}
-            speed={500}
-            pagerColor="#000"
-            activePagerColor="#fff"
-          >
-            <View style={[styles.card]}>
+         <ScrollView horizontal={true} pagingEnabled={true} scrollEnabled={false} ref="_scrollView">
+            <View style={[styles.card, {width: viewportWidth}]}>
               <Text>Who?</Text>
               <TextInput
                 style={styles.textInput}
                 onChangeText={(text) => this.setState({who: text})}
                 value={this.state.who}
               />
+              <Button
+                onPress={this.handleNextPress}
+                title="Next"
+                color="#5844ed"
+                accessibilityLabel="Go to next step"
+              />
             </View>
-            <View style={[styles.card]}>
+            <View style={[styles.card, {width: viewportWidth}]}>
               <Text>What?</Text>
               <TextInput
                 style={styles.textInput}
                 onChangeText={(text) => this.setState({what: text})}
                 value={this.state.what}
               />
+              <Button
+                onPress={this.handleNextPress}
+                title="Next"
+                color="#5844ed"
+                accessibilityLabel="Go to next step"
+              />
+              <Button
+                onPress={this.handleBackPress}
+                title="Back"
+                color="#5844ed"
+                accessibilityLabel="Go to previous step"
+              />
             </View>
-            <View style={[styles.card]}>
+            <View style={[styles.card, {width: viewportWidth}]}>
               <Text>When?</Text>
               <TextInput
                 style={styles.textInput}
                 onChangeText={(text) => this.setState({when: text})}
                 value={this.state.when}
               />
+              <Button
+                onPress={this.handleNextPress}
+                title="Next"
+                color="#5844ed"
+                accessibilityLabel="Go to next step"
+              />
+              <Button
+                onPress={this.handleBackPress}
+                title="Back"
+                color="#5844ed"
+                accessibilityLabel="Go to previous step"
+              />
             </View>
-            <View style={[styles.card]}>
+            <View style={[styles.card, {width: viewportWidth}]}>
               <Text>Where?</Text>
               <TextInput
                 style={styles.textInput}
                 onChangeText={(text) => this.setState({where: text})}
                 value={this.state.where}
               />
+              <Button
+                onPress={this.handleBackPress}
+                title="Back"
+                color="#5844ed"
+                accessibilityLabel="Go to previous step"
+              />
             </View>
-          </Carousel>
+          </ScrollView>
       );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    width: 500, 
-    height: 500
+    flex: 1
   },
   card: {
     flex: 1,
